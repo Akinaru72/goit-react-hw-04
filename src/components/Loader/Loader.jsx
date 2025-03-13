35bf4b54c7d5975f20094546cc19802a8d418fa1
Volodymyr <akinaru3@gmail.com>
2025-03-14 01:56:43 +0200

    1

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,6 @@
+import { ClipLoader } from "react-spinners";
+import css from "./Loader.module.css";
+
+const Loader = () => <ClipLoader className={css.loader} color="#36d7b7" />;
+
+export default Loader;
